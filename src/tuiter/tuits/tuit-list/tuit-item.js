83ea02ed53d1410cac74185710fcd020eede9c0a
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-21 18:16:08 -0500

    a8 NODE fa22

--- src/tuiter/tuits/tuit-list/tuit-item.js
+++ src/tuiter/tuits/tuit-list/tuit-item.js
@@ -22,7 +22,7 @@ const TuitItem = (
     const deleteTuitHandler = (id) => {
         dispatch(deleteTuitThunk(id));
     }
-    console.log(post.userName + '')
+    console.log(post.handle)
     return(
         <li className="list-group-item">
             <div className="row">
