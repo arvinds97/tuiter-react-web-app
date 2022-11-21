fbf1188bd90e912b1124d08ef39b280cf8382811
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-21 17:06:29 -0500

    a8 NODE fa22

--- src/tuiter/tuits/tuit-stats/tuit-stat-item.js
+++ src/tuiter/tuits/tuit-stats/tuit-stat-item.js
@@ -1,93 +1,47 @@
 import React from "react";
-import '../styles.css';
-import {likeToggle} from "../tuits-reducer";
 import {useDispatch} from "react-redux";
+import {likeTuit} from "../tuits-reducer";
 import {updateTuitThunk} from "../../../services/tuits-thunks";
 
-const TuitStats = ({
-                       post = {
-                           _id: 567,
-                           topic: "Space",
-                           userName: 'Elon Musk',
-                           time: '23h',
-                           title: 'Countdown: Inspiration4 Mission to Space | Netflix Official Site',
-                           image: 'elon.jpeg',
-                           liked: true,
-                           disliked: true,
-                           replies: 890,
-                           retuits: '3.5K',
-                           likes: 37.5,
-                           dislikes: 464,
-                           handle: '@elonmusk',
-                           tuit: 'From training to launch to landing, this all-access docuseries rides along with the Inspiration4 crew on the first all-civilian orbital space mission.',
-                       }
-                   }) => {
+const TuitStatItem = ({post}) => {
     const dispatch = useDispatch();
-    let updateLikes;
-    let updateDislikes;
-    const toggleLike = (post) => {
-        dispatch(likeToggle(post))
-    }
+    const likeToggleHandler = (id) => {
+        dispatch(updateTuitThunk({
+            ...post,
+            likes:  post.liked ? post.likes - 1 : post.likes + 1,
+            liked: !post.liked
+        }))
+    };
+
+    const dislikeToggleHandler = (id) => {
+        dispatch(updateTuitThunk({
+            ...post,
+            dislikes:  post.disliked ? post.dislikes - 1 : post.dislikes + 1,
+            disliked: !post.disliked
+        }))
+    };
     return (
-        <div className="wd-float-left wd-symbols">
-            <div className="wd-float-left">
-                <i className="bi bi-chat wd-grey-text"></i>
-                <a className="wd-icons-text" href="">
-                    {post.replies}
-                </a>
-            </div>
-            <div className="wd-float-left">
-                <i className="bi bi-arrow-repeat wd-grey-text"></i>
-                <a className="wd-icons-text" href="">
-                    {post.retuits}
-                </a>
-            </div>
-            <div className="wd-float-left">
-                <i className={post.liked?"bi bi-heart-fill" : "bi bi-heart wd-grey-text"}
-                   style={{color: post.liked? 'red': ''}}
-                   onClick={() => {
-                       if (post.liked){
-                           updateLikes = post.likes - 1
-                       }
-                       else {
-                           updateLikes = post.likes + 1
-                       }
-                       dispatch(updateTuitThunk({
-                           ...post,
-                           likes: updateLikes,
-                           liked: !post.liked
-                       }))
-                   }}>
-                </i>
-                <a className="wd-icons-text wd-heart" href="">
-                    {post.likes}
-                </a>
-            </div>
-            <div className="wd-float-left">
-                <i className={post.disliked?"bi bi-hand-thumbs-down-fill" : "bi bi-hand-thumbs-down wd-grey-text"}
-                   style={{color: post.disliked? 'royalblue': ''}}
-                   onClick={() => {
-                       if (post.disliked){
-                           updateDislikes = post.dislikes - 1
-                       }
-                       else {
-                           updateDislikes = post.dislikes + 1
-                       }
-                       dispatch(updateTuitThunk({
-                           ...post,
-                           dislikes: updateDislikes,
-                           disliked: !post.disliked
-                       }))
-                   }}>
-                </i>
-                <a className="wd-icons-text wd-heart" href="">
-                    {post.dislikes}
-                </a>
-            </div>
-            <div className="wd-float-left">
-                <i className="bi bi-share wd-grey-text"></i>
-            </div>
+        <div className="row border-0 mt-2">
+                <span className="col-3">
+                    <i className="ps-4 fa-regular fa-comment"> </i>
+                    <span className="ps-2">{post.replies}</span>
+                </span>
+            <span className="col-3">
+                    <span className="ps-4 fas fa-retweet"> </span>
+                    <span className="ps-2">{post.retuits}</span>
+                </span>
+            <span className="col-3">
+                    <span className= "ps-4">
+                   <span className={`ps-2 pe-1 small ${post.liked? 'text-danger fa-solid fa-heart' :'fa-regular fa-heart'}`}
+                         onClick={() => {likeToggleHandler(post._id)}}></span>
+                        {post.likes}</span>
+
+                </span>
+            <span className="col-3">
+                    <span className="ps-4 fa fa-share-alt"></span>
+                </span>
         </div>
-    )
-}
-export default TuitStats;
+    );
+};
+export default TuitStatItem;
+
