9da2b42de23c1a9cf4ccbd722f8dbee5e9f55f68
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-21 23:25:27 -0500

    a8 REACT fa22

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
-const TUITS_API = 'https://tuiter-node-server-app-arv.herokuapp.com/api/tuits';
-
+//const TUITS_API = 'https://tuiter-node-server-app-arv.herokuapp.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+console.log("label :::",API_BASE);
+const TUITS_API = `${API_BASE}/tuits`;
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
@@ -11,7 +13,7 @@ export const findTuits = async () => {
     return tuits;
 
 }
-export const deleteTuit = async (tuit) => {
+export const deleteTuit = async (tid) => {
     const response = await axios
         .delete(`${TUITS_API}/${tid}`)
     return response.data;
