53b63dc51fc37003155bfea019986a81b9f4cdf4
Arvind Sudheer <arvind.sudheer@gmail.com>
2022-11-30 14:00:21 -0500

    a9 Mongo fa22

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 //const TUITS_API = 'https://tuiter-node-server-app-arv.herokuapp.com/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+//const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = 'https://tuiter-node-server-app-arv.herokuapp.com/api';
 console.log("label :::",API_BASE);
 const TUITS_API = `${API_BASE}/tuits`;
 export const createTuit = async (tuit) => {
